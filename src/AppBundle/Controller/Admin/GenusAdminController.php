cc22a2d330877b4ee4c449d4c5531d841a3f4d8b
Ruan Silva <ruanluiz.as@gmail.com>
2017-11-21 11:17:24 -0200

    modulo6 - Step3 Process that Form!

--- src/AppBundle/Controller/Admin/GenusAdminController.php
+++ src/AppBundle/Controller/Admin/GenusAdminController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller\Admin;
 use AppBundle\Form\GenusFormType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * @Route("/admin")
@@ -28,11 +29,19 @@ class GenusAdminController extends Controller
     /**
      * @Route("/genus/new", name="admin_genus_new")
      */
-    public function newAction()
+    public function newAction(Request $request)
     {
         // let's go to work!
         $form = $this->createForm(GenusFormType::class);
 
+        // only handles data on POST
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            dump($request);
+            dump($form);
+            dump($form->getData());die;
+        }
         return $this->render('admin/genus/new.html.twig', [
             'genusForm' => $form->createView()
         ]);
